877adbceb041757dc8334bbbac1984ea861e9b5c
fran-scala <scala0francesco@gmail.com>
2023-09-12 23:59:55 +0200

    basic entangler

--- advanced/VQNN_basic_entangler.py
+++ advanced/VQNN_basic_entangler.py
@@ -13,6 +13,8 @@ n_qubits = 7  # equal to the number of features
 layers = 3
 sublayers = 1
 params_per_sublayer = 3 * n_qubits
+seed = 1234
+
 device = qml.device("default.qubit.jax", wires=n_qubits)
 
 
@@ -27,20 +29,6 @@ def encode_data_nonlinear(x):
 
         qml.RX(jnp.arctan(x[i])*2, wires=i)
         qml.RZ(x[i]**2*jnp.pi, wires=i)
-
-
-
-def basic_ansatz(thetas,):
-    '''
-    thetas - Parameters of the QNN
-
-    Applies the Anstaz to the circuit. 
-    The ansatz is composed by a repition of sublayers.
-    '''
-    k = 0
-    for lay in range(sublayers):
-        qml.BasicEntanglerLayers(thetas[k:params_per_sublayer + k])
-        k += params_per_sublayer
 
 
 @jax.jit
@@ -76,7 +64,7 @@ def optimizer_update(opt_state, params, x, y, optimizer, qnn):
 
 
 @qml.qnode(device,interface='jax')
-def circuit_nonlinear(x, thetas):
+def circuit_basic_entangler(x, thetas):
     '''
     x - input features
     thetas - Parameters of thn QNN
@@ -88,13 +76,22 @@ def circuit_nonlinear(x, thetas):
     for lay in range(layers):
         encode_data_nonlinear(x,)
         qml.Barrier(wires=range(n_qubits),only_visual=True)
-        basic_ansatz(thetas[j:j+(params_per_sublayer)*sublayers],)
-        j += (params_per_sublayer)*sublayers
+        qml.BasicEntanglerLayers(thetas[j], wires=range(n_qubits),)
+        j += 1
         qml.Barrier(wires=range(n_qubits),only_visual=True)
     qml.Barrier(wires=range(n_qubits),only_visual=True)
 
     return qml.expval(qml.PauliZ(0))
 
+# import matplotlib.pyplot as plt
+# layers = 3
+# sublayers = 1
+# params_per_sublayer = n_qubits
+# key = jax.random.PRNGKey(seed)
+# initial_params = jax.random.normal(key, shape=(layers ,sublayers,params_per_sublayer))
+# print(initial_params)
+# qml.draw_mpl(circuit_basic_entangler,expansion_strategy='device')([1,0,1,1,1,1,0,0],initial_params,)
+# plt.savefig('./basic_entangler_circuit.pdf')
 
 @jax.jit
 def calculate_mse_cost(X, y, theta):
@@ -126,7 +123,6 @@ def optimizer_update(opt_state, params, x, y, ):
     updates, opt_state = optimizer.update(grads, opt_state)
     params = optax.apply_updates(params, updates)
     return params, opt_state, loss
-
 
 df_with_outliers = pd.read_csv('/g100/home/usertrain/a08tra32/QuHack4IA_VQNN-track/dataset/dataset_without_outliers_without_feature.csv')
 
@@ -161,7 +157,7 @@ for layers in range(min_layers, max_layers + 1):
         # Jax jit and vmap speed up the computational times of the circuit
 
         params_per_sublayer = n_qubits
-        qnn_batched = jax.vmap(circuit_nonlinear, (0, None,))
+        qnn_batched = jax.vmap(circuit_basic_entangler, (0, None,))
         qnn = jax.jit(qnn_batched)
 
         # Lists to save data
@@ -172,7 +168,7 @@ for layers in range(min_layers, max_layers + 1):
 
         # Creating the initial random parameters for the QNN
         key = jax.random.PRNGKey(seed)
-        initial_params = jax.random.normal(key, shape=((params_per_sublayer) * layers * sublayers,))
+        initial_params = jax.random.normal(key, shape=(layers ,sublayers,params_per_sublayer))
         key = jax.random.split(key)[0]
         params = jnp.copy(initial_params)
 
